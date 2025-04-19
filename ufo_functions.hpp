cae1cc28453303fc9ec5b2088579a9ce700cad6d
volkan <volkannecipoglu@proton.me>
2025-04-19 18:37:11 +0300

    Function to show the incorrect guesses and the current progress

--- ufo_functions.hpp
+++ ufo_functions.hpp
@@ -8,5 +8,6 @@
 
 void greet();
 void display_misses(int misses);
-void end_game();
+void end_game(std::string answer, std::string codeword);
+void display_status(std::vector<char> incorrect, std::string answer);
 #endif // UFO_FUNCTIONS_H
