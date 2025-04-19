2ce750bdcfadfde9c3d1dc906211e59fa0863af0
volkan <volkannecipoglu@proton.me>
2025-04-19 17:37:53 +0300

    .

---
+++ ufo_functions.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+
+// Greeting message for player
+void greeting();
+    std::cout << "=============\n";
+    std::cout << "UFO: The Game\n";
+    std::cout << "=============\n";
+    std::cout << "Instructions: save your friend from alien abduction by guessing the letters in the codeword.\n\n";
---
+++ ufo_functions.h
@@ -0,0 +1,8 @@
+//
+// Created by volkan on 4/19/25.
+//
+
+#ifndef UFO_FUNCTIONS_H
+#define UFO_FUNCTIONS_H
+
+#endif //UFO_FUNCTIONS_H
