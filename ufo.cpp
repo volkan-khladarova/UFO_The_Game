0927a0325b8565c3c8ed223c28fef2591c834256
volkan <volkannecipoglu@proton.me>
2025-04-19 18:17:07 +0300

    Add player needs to guess text, answer initially all underscores, vector to keep track of incorrect guessed letters and boolean to check if the current guess was correct

--- ufo.cpp
+++ ufo.cpp
@@ -5,5 +5,20 @@
 int main() {
     greet();
 
+    // The secret word the player needs to guess
+    std::string codeword = "codecademy";
+
+    // The current state of the guessed word (initially all underscores)
+    std::string answer = "__________";
+
+    // Number of incorrect guesses
+    int misses = 0;
+
+    // Vector to keep track of incorrect guessed letters
+    std::vector<char> incorrect_guesses;
+
+    //Boolean to check if the current guess was correct
+    bool guess = false;
+
     return 0;
 }
