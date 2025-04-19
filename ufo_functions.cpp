76d61c08bf1435c5631e8e2b8bc78236ae220630
volkan <volkannecipoglu@proton.me>
2025-04-19 17:51:04 +0300

    Changed name

--- ufo_functions.cpp
+++ ufo_functions.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 
 // Greeting message for player
-void greeting() {
+void greet() {
     std::cout << "=============\n";
     std::cout << "UFO: The Game\n";
     std::cout << "=============\n";
