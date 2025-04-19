0cb4a1fe1a0480dbdbe8e6f1101fa060854faa1b
volkan <volkannecipoglu@proton.me>
2025-04-19 18:51:22 +0300

    Fixed some space

--- ufo_functions.hpp
+++ ufo_functions.hpp
@@ -10,4 +10,5 @@ void greet();
 void display_misses(int misses);
 void end_game(std::string answer, std::string codeword);
 void display_status(std::vector<char> incorrect, std::string answer);
+
 #endif // UFO_FUNCTIONS_H
